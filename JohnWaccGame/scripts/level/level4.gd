feb2781cc5e9d481ecff1f88c7acec7f7db8c736
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-03 15:25:19 -0400

    fixed some shit

---
+++ JohnWaccGame/scripts/level/level4.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var player = $Player.get_player()
+@onready var game_manager: Control = %GameManager
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if save_system.checkpoint_data.stage != 4:
+		save_system.checkpoint_save(player.position.x, player.position.y)
+	save_system.current_data.stage = 4
+	audio_manager.play_music("TidalTempest")
+	# create a new checkpoint save at the start of the level
+	save_system.checkpoint_load(player)
+	game_manager._ready()
