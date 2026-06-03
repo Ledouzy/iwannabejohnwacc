068f0b3ee025a7cd2e49f99af71b8da23160e4f1
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-03 15:17:01 -0400

    oh shit there's a lot of shit

--- JohnWaccGame/scripts/logic/audio_manager.gd
+++ JohnWaccGame/scripts/logic/audio_manager.gd
@@ -2,10 +2,12 @@ extends Node
 
 var active_music_stream: AudioStreamPlayer
 var active_sound_stream: AudioStreamPlayer2D
+var active_bgs_stream: AudioStreamPlayer
 
 @export_group("Main")
 @export var music: Node
 @export var sounds: Node
+@export var BGS: Node
 @export_group("UI")
 @export var confirm_sfx: AudioStreamPlayer
 @export var move_sfx: AudioStreamPlayer
@@ -18,6 +20,14 @@ func play_music(audio_name: String, from_position: float = 0.0, restart: bool = 
 		active_music_stream.stop()
 	active_music_stream = music.get_node(audio_name)
 	active_music_stream.play(from_position)
+	
+func play_bgs(audio_name: String, from_position: float = 0.0, restart: bool = false) -> void:
+	if !restart and active_bgs_stream and active_bgs_stream.name == audio_name:
+		return
+	elif active_bgs_stream and active_bgs_stream.name != audio_name:
+		active_bgs_stream.stop()
+	active_bgs_stream = BGS.get_node(audio_name)
+	active_bgs_stream.play(from_position)
 
 func play_sfx(audio_name: String, from_position: float = 0.0, sound_position: Vector2 = Vector2(0,0)) -> void:
 	active_sound_stream = sounds.get_node(audio_name)
