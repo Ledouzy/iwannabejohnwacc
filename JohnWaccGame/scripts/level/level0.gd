41e0ec2e6843344365e138719c64a353178a3b3d
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-04 12:44:35 -0400

    guh?

--- JohnWaccGame/scripts/level/level0.gd
+++ JohnWaccGame/scripts/level/level0.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-@onready var player: CharacterBody2D = $Player.get_player()
+@onready var player = $Player.get_player()
+@onready var game_manager: Control = %GameManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_manager.play_music("TheBoyWhoHadWings")
-	save_system.checkpoint_load(player)
+	if save_system.checkpoint_data.stage != 0:
+		save_system.checkpoint_save(player.position.x, player.position.y)
 	save_system.current_data.stage = 0
+	audio_manager.play_music("TheBoyWhoHadWings")
+	# create a new checkpoint save at the start of the level
+	save_system.checkpoint_load(player)
+	game_manager._ready()
