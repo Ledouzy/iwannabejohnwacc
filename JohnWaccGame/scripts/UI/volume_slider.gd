db5d9d67d3a427fe8f87dd1e3b5416f968b6e56e
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-05 14:29:04 -0400

    SETTINGS WORKS!!!!

--- JohnWaccGame/scripts/UI/volume_slider.gd
+++ JohnWaccGame/scripts/UI/volume_slider.gd
@@ -7,7 +7,11 @@ var bus_index: int
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
-	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+	value = global_manager.loaded_settings.get(bus_name.to_lower())
+	#AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
 	
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+	print("changing ", bus_name, " to ", value)
+	global_manager.update_settings()
+	global_manager.save_settings()
--- JohnWaccGame/scripts/global/global_manager.gd
+++ JohnWaccGame/scripts/global/global_manager.gd
@@ -1,5 +1,88 @@
 extends Node
 
+var settings_location = "user://settings.json"
+
+var default_settings = {
+	"master": 0.4,
+	"music": 1.0,
+	"sfx": 1.0,
+	"bgs": 1.0
+}
+
+var loaded_settings = {
+	"master": 0.4,
+	"music": 1.0,
+	"sfx": 1.0,
+	"bgs": 1.0
+}
+
+func load_settings():
+	if FileAccess.file_exists(settings_location):
+		print("found settings file.")
+		var file = FileAccess.open(settings_location, FileAccess.READ)
+		var data = file.get_var()
+		file.close()
+		
+		var settings_data = data.duplicate()
+		
+		print("Master: ", settings_data.master, " Music: ", settings_data.music, " SFX: ", settings_data.sfx, " BGS: ", settings_data.bgs)
+		
+		loaded_settings.master = settings_data.master
+		loaded_settings.music = settings_data.music
+		loaded_settings.sfx = settings_data.sfx
+		loaded_settings.bgs = settings_data.bgs
+	# TODO: add the rest
+	else:
+		print("Creating new settings file.")
+		var file = FileAccess.open(settings_location, FileAccess.WRITE)
+		file.store_var(default_settings.duplicate())
+		file.close()
+		loaded_settings = default_settings.duplicate()
+	print("is this even doing this")
+	# now that we loaded data, apply the changes
+	var bus_index
+	
+	bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(loaded_settings.master))
+	print("updated master to ", loaded_settings.master)
+	
+	bus_index = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(loaded_settings.music))
+	
+	bus_index = AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(loaded_settings.sfx))
+	
+	bus_index = AudioServer.get_bus_index("BGS")
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(loaded_settings.bgs))
+	
+func update_settings():
+	print("updating settings")
+	var bus_index
+	
+	bus_index = AudioServer.get_bus_index("Master")
+	print("Master: ", AudioServer.get_bus_volume_linear(bus_index))
+	loaded_settings.master = AudioServer.get_bus_volume_linear(bus_index)
+	
+	bus_index = AudioServer.get_bus_index("Music")
+	print("Music: ", AudioServer.get_bus_volume_linear(bus_index))
+	loaded_settings.music = AudioServer.get_bus_volume_linear(bus_index)
+	
+	bus_index = AudioServer.get_bus_index("SFX")
+	print("SFX: ", AudioServer.get_bus_volume_linear(bus_index))
+	loaded_settings.sfx = AudioServer.get_bus_volume_linear(bus_index)
+	
+	bus_index = AudioServer.get_bus_index("BGS")
+	print("BGS: ", AudioServer.get_bus_volume_linear(bus_index))
+	loaded_settings.bgs = AudioServer.get_bus_volume_linear(bus_index)
+	
+func save_settings():
+	var file = FileAccess.open(settings_location, FileAccess.WRITE)
+	file.store_var(loaded_settings.duplicate())
+	file.close()
+
+func _ready() -> void:
+	load_settings()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
