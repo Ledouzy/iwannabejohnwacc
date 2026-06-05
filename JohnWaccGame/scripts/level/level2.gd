2abf77436184b5357033da7d62de33f017cca726
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-05 15:05:44 -0400

    eeeeeeeehhhhhhhh

--- JohnWaccGame/scripts/level/level2.gd
+++ JohnWaccGame/scripts/level/level2.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	if save_system.checkpoint_data.stage != 2:
 		save_system.checkpoint_save(player.position.x, player.position.y)
 	save_system.current_data.stage = 2
-	audio_manager.play_music("TheBoysWhoHadWings")
+	audio_manager.play_music("StarlightZone")
 	# create a new checkpoint save at the start of the level
 	save_system.checkpoint_load(player)
 	game_manager._ready()
