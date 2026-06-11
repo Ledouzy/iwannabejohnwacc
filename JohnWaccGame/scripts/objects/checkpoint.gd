4b1ef5a47da8432a5c747ae2f4ff1f7eb15cf683
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-11 16:11:03 -0400

    commented checkpoint

--- JohnWaccGame/scripts/objects/checkpoint.gd
+++ JohnWaccGame/scripts/objects/checkpoint.gd
@@ -3,4 +3,5 @@ extends Area2D
 # IMPORTANT: CHECKPOINTS NOT BUT BE A CHILD OF SOMETHING THAT HAS POSITION NOT EQUAL TO 0
 
 func _on_body_entered(body: Node2D) -> void:
+	# save to the checkpoint slot
 	save_system.checkpoint_save(self.position.x, self.position.y)
