b2945a47351c896b603eabfa8fa68315fc734286
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-06-15 18:08:59 -0400

    i stop here for today

--- JohnWaccGame/scripts/UI/options_screen.gd
+++ JohnWaccGame/scripts/UI/options_screen.gd
@@ -1,28 +1,32 @@
 extends Control
 
+# reference to the other screens
 @onready var display_options_screen: Control = $display_options_screen
 @onready var sound_options_screen: Control = $sound_options_screen
 @onready var input_options_screen: Control = $input_options_screen
+# reference to the display button
 @onready var display_button: Button = $UI_elements/VBoxContainer2/VBoxContainer/DisplayButton
 
 func focused():
 	self.get_parent().release_focus()
 	display_button.grab_focus()
 
+# show the display options screen
 func _on_display_button_pressed() -> void:
 	display_options_screen.visible = true
 	display_options_screen.focused()
 
-
+# show the sound options screen
 func _on_sound_button_pressed() -> void:
 	sound_options_screen.visible = true
 	sound_options_screen.focused()
 
+# show the input options screen
 func _on_input_button_pressed() -> void:
 	input_options_screen.visible = true
 	input_options_screen.focused()
 
-
+# hides the options menu
 func _on_back_button_pressed() -> void:
 	self.visible = false
 	self.get_parent().focused()
--- JohnWaccGame/scripts/UI/pause_screen.gd
+++ JohnWaccGame/scripts/UI/pause_screen.gd
@@ -1,41 +1,49 @@
 extends Control
 
+# is the menu opened
+var opened : bool = false
+
+# reference to the options screen
 @onready var options_screen: Control = $options_screen
-var opened
+# reference to the resume button
 @onready var resume_button: Button = $UI_elements/VBoxContainer2/VBoxContainer/ResumeButton
+# reference to the player
 @onready var player = $"../../../Player"
 
 func focused():
 	resume_button.grab_focus()
 
 func _on_resume_button_pressed() -> void:
+	# unpause the game and hide the screen
 	get_tree().paused = false
 	self.visible = false
 
 func _on_option_button_pressed() -> void:
+	# show the options screen and focus it
 	options_screen.visible = true
 	options_screen.focused()
 
 func _on_title_button_pressed() -> void:
-	# probably have a confirmation popup
+	# TODO: probably have a confirmation popup
+	# unpause the game
 	get_tree().paused = false
+	# load the title screen
 	scene_manager.go_to_title(get_tree())
 
 func _process(delta: float) -> void:
+	# check for pause input
 	if Input.is_action_just_pressed("pause"):
 		if !opened:
+			# if not opened, open it and focus the screen
 			opened = true
 			focused()
 		else:
+			# close the screen
 			opened = false
 			_on_resume_button_pressed()
 
 
 func _on_retry_button_pressed() -> void:
-	#if player != null:
-	#	save_system.checkpoint_load(player)
-	#else:
-	#	print("player is null")
-	
+	# reload the scene at the checkpoint and unpause the game
 	scene_manager.reload_scene()
 	get_tree().paused = false
