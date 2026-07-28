d4f07d88e3ca0bacd69024c78dba2bcf046ab1f4
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-07-28 16:24:37 -0400

    terrible sprites and tilesets changes

--- JohnWaccGame/scripts/character/player_side.gd
+++ JohnWaccGame/scripts/character/player_side.gd
@@ -435,7 +435,7 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("Shrug")
 			
 			# wait for shrug animation end
-			await animated_sprite.animation_finished
+			await animated_sprite.animation_changed
 			
 			shruganim = false
 			
